bf7ae4e0b3bccaaf12dc1b9b6b6f51d7f4bbe320
nangdev <zovl5546@naver.com>
2025-01-23 14:53:12 +0900

    init : 프로젝트 설정 및 초기 프로젝트 구축

---
+++ backend/talktalkcare/src/main/java/com/talktalkcare/TalktalkcareApplication.java
@@ -0,0 +1,13 @@
+package com.talktalkcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktalkcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalktalkcareApplication.class, args);
+    }
+
+}
---
+++ backend/talktalkcare/src/test/java/com/talktalkcare/TalktalkcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talktalkcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalktalkcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
