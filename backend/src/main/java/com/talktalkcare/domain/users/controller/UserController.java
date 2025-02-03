b8f6cfce6c8a67874094e293ea2aeb422a03e279
nangdev <zovl5546@naver.com>
2025-02-03 17:39:12 +0900

    feat : 회원가입 구현 완료 및 로그인 구현 완료, 회원가입, 로그인 에러코드 추가

--- backend/src/main/java/com/talktalkcare/domain/users/controller/UserController.java
+++ backend/src/main/java/com/talktalkcare/domain/users/controller/UserController.java
@@ -2,7 +2,11 @@ package com.talktalkcare.domain.users.controller;
 
 import com.talktalkcare.common.response.Api;
 import com.talktalkcare.domain.users.dto.LoginDto;
+import com.talktalkcare.domain.users.dto.ProfileImagReq;
+import com.talktalkcare.domain.users.dto.ProfileImageResp;
 import com.talktalkcare.domain.users.dto.UserDto;
+import com.talktalkcare.domain.users.entity.User;
+import com.talktalkcare.domain.users.service.S3Service;
 import com.talktalkcare.domain.users.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final S3Service s3Service;
 
     @GetMapping("/check-id")
     public Api<Void> checkUserId(@RequestParam(name = "userLoginId") String userLoginId) {
@@ -32,6 +37,18 @@ public class UserController {
     public Api<Void> login(@RequestBody LoginDto request, HttpSession session, HttpServletResponse response) {
         userService.login(request, session, response);
         return Api.OK();
+    }
+
+    @PostMapping("/upload-profile")
+    public Api<ProfileImageResp> uploadProfileImage(@RequestBody ProfileImagReq profileImagReq) {
+        return Api.OK(userService.updateProfileImage(profileImagReq));
+    }
+
+    @GetMapping("/profile-image")
+    public Api<ProfileImageResp> getProfileImageUrl(@RequestParam(name = "userLoginId") String userLoginId) {
+        User user = userService.getUser(userLoginId);
+        ProfileImageResp profileImageResp = s3Service.getFileUrl(user.getS3FileName());
+        return Api.OK(profileImageResp);
     }
     
 }
---
+++ backend/src/main/java/com/talktalkcare/domain/users/dto/ProfileImageResp.java
@@ -0,0 +1,8 @@
+package com.talktalkcare.domain.users.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ProfileImageResp {
+    private String imageUrl;
+}
