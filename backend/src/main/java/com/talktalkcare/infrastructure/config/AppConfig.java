4888ad190e00f3ad235f354d70fe49732d3bd255
yoon <yoon733@naver.com>
2025-02-10 23:23:35 +0900

    rename : Config 파일명 수정

--- backend/src/main/java/com/talktalkcare/infrastructure/config/AppConfig.java
+++ backend/src/main/java/com/talktalkcare/infrastructure/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class GlobalConfig {
+public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
