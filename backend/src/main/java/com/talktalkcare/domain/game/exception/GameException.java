1ec3d8e318759b385ce402a444d40bfa315389e4
eoslovy <ydg0630@naver.com>
2025-02-10 15:40:06 +0900

    fix: merge conflict solve

--- backend/src/main/java/com/talktalkcare/domain/game/exception/GameException.java
+++
@@ -1,8 +0,0 @@
-//package com.talktalkcare.domain.game.exception;
-//
-//public class GameException extends ApplicationException {
-//    public GameException(ErrorCodeInterface errorCode) {
-//        super(errorCode);
-//    }
-//
-//}
