d20cdc2fc13791aa96efec07616eb862bb585376
eoslovy <ydg0630@naver.com>
2025-02-11 17:10:53 +0900

    refactor: 주석 제거

--- backend/src/main/java/com/talktalkcare/domain/games/dto/GameRecordDto.java
+++ backend/src/main/java/com/talktalkcare/domain/games/dto/GameRecordDto.java
@@ -1,18 +1,12 @@
 package com.talktalkcare.domain.games.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
 public class GameRecordDto {
-
     private int userId;
     private int gameId;
     private int score;
-
 }
