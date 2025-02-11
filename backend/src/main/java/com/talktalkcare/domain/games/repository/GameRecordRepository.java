37b72c51edad80218486f2ddc662fa9d277c96fb
eoslovy <ydg0630@naver.com>
2025-02-11 17:11:08 +0900

    refactor: 엔티티명 수정

--- backend/src/main/java/com/talktalkcare/domain/games/repository/GameRecordRepository.java
+++ backend/src/main/java/com/talktalkcare/domain/games/repository/GameRecordRepository.java
@@ -1,12 +1,8 @@
 package com.talktalkcare.domain.games.repository;
 
-import com.talktalkcare.domain.games.entity.DailyCategoryScore;
-import com.talktalkcare.domain.games.entity.GameList;
+import com.talktalkcare.domain.games.entity.GameScorePerDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface GameRecordRepository extends JpaRepository<DailyCategoryScore, Long> {
-
+public interface GameRecordRepository extends JpaRepository<GameScorePerDay, Long> {
 
 }
