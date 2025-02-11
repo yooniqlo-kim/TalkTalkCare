bf475b19ed22d3a6c88bea4c52b991e364d59cd8
eoslovy <ydg0630@naver.com>
2025-02-11 17:08:27 +0900

    refactor: 게임 기록 저장 기능 수정

--- backend/src/main/java/com/talktalkcare/domain/games/controller/GameController.java
+++ backend/src/main/java/com/talktalkcare/domain/games/controller/GameController.java
@@ -2,7 +2,7 @@ package com.talktalkcare.domain.games.controller;
 
 import com.talktalkcare.common.response.Api;
 import com.talktalkcare.domain.games.dto.GameRecordDto;
-import com.talktalkcare.domain.games.service.GameRecordService;
+import com.talktalkcare.domain.games.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,22 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class GameController {
 
-    private final GameRecordService gameRecordService;
+    private final GameService gameService;
 
-    @PostMapping("/game-result")
-    public Api<String> saveTestResult(@RequestBody GameRecordDto dto) {
-        gameRecordService.saveGameResult(dto);
-        return Api.OK("게임 결과 전송 완료");
+    @PostMapping("/save-result")
+    public Api<Void> saveTestResult(@RequestBody GameRecordDto dto) {
+        gameService.saveGameResult(dto);
+        return Api.OK();
     }
-
-/*    @GetMapping("/category-average")
-    public Api<?> getCategoryAverageScore(@PathVariable Integer userId) {
-        try {
-            CategoryAvgScoreDto categoryAvgScore = gameRecordService.getCategoryAverageScoreByGameId(userId);
-            return ResponseEntity.ok(categoryAvgScore);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ApiResponse("Failed to retrieve category average score"));
-        }
-    }*/
 }
