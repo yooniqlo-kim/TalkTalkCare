727cdeeaf00c44c00f0f64faedc46fb3d7af5a4d
jooo <wndnjs510@naver.com>
2025-02-12 16:40:12 +0900

    feat:footer 추가

--- frontend/talktalkcare/src/components/layout/Layout.tsx
+++ frontend/talktalkcare/src/components/layout/Layout.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.tsx';
+import Footer from './Footerbar.tsx';
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
