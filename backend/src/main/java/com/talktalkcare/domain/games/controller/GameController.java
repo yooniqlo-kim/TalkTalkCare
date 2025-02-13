c5675e409c1e8ed1643630018f7a3d22a67df013
eoslovy <ydg0630@naver.com>
2025-02-13 13:49:27 +0900

    feat: 저번 달 카테고리 별 게임 평균 기록 가져오는 기능 구현

--- backend/src/main/java/com/talktalkcare/domain/games/controller/GameController.java
+++ backend/src/main/java/com/talktalkcare/domain/games/controller/GameController.java
@@ -1,10 +1,13 @@
 package com.talktalkcare.domain.games.controller;
 
 import com.talktalkcare.common.response.Api;
+import com.talktalkcare.domain.games.dto.GameCategoryScoreDto;
 import com.talktalkcare.domain.games.dto.GameRecordDto;
 import com.talktalkcare.domain.games.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/games")
@@ -17,5 +20,11 @@ public class GameController {
     public Api<Void> saveTestResult(@RequestBody GameRecordDto dto) {
         gameService.saveGameResult(dto);
         return Api.OK();
+    }
+
+    @GetMapping("monthly-stats/{userId}")
+    public Api<List<GameCategoryScoreDto>> getMonthlyGameStats(@PathVariable Integer userId) {
+        List<GameCategoryScoreDto> monthlyStats = gameService.getMonthlyGameStatsByCategory(userId);
+        return Api.OK(monthlyStats);
     }
 }
