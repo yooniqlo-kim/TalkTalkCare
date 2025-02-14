f644e6c869d607803be40ce8cd3224a660d1bec7
jooo <wndnjs510@naver.com>
2025-02-14 09:41:06 +0900

    fix: game css 수정

---
+++ frontend/talktalkcare/src/pages/GamePages/page/Logic/LogicalGame.ts
@@ -0,0 +1,11 @@
+import LogicGame1 from './PatternGame';
+
+export default [
+  { id: '1',
+    name: '숫자 패턴 찾기',
+    description: '숫자의 규칙을 찾아 빈칸을 채우세요!',
+    icon: '🔢',
+    skill: '논리력',
+    component: LogicGame1 },
+//   { id: '2', name: 'Logic Game 2', description: '논리력과 사고력을 함께 훈련하는 게임입니다.', component: LogicGame2 }
+];
